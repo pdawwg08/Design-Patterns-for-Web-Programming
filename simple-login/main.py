d21389862a2cbfcd831cbfffc90464859190ab0b
pdawwg08 <pdawwg08@yahoo.com>
2015-02-12 15:43:56 -0700

    fixing form

--- simple-login/main.py
+++ simple-login/main.py
@@ -13,17 +13,17 @@ class MainHandler(webapp2.RequestHandler):
         <title>Simple Form</title>
     </head>
     <body>'''
-        page_body = '''<form method="GET">
-            <label>Name: </label><input type="text" name="user"/>
-            <label>Email: </label><input type="email" name="email"/>
-            <input type="radio" value="Male" name="sex"/>
-            <input type="radio" value="Female" name="sex"/>
-            <label>Age: </label><input type="number" name="age"/>
-            <label>Interest: </label><select name="interest" multiple>
+        page_form = '''<form method="GET">
+            <label>Name: </label><input type="text" name="user"/></br>
+            <label>Email: </label><input type="email" name="email"/></br>
+            <label>Male </label><input type="radio" value="Male" name="sex"/></br>
+            <label>Female </label><input type="radio" value="Female" name="sex"/></br>
+            <label>Age: </label><input type="number" name="age"/></br>
+            <label>Interest: </label><select name="interest">
                 <option value="sports">Sports</option>
                 <option value="movies">Movies</option>
                 <option value="music">Music</option>
-            </select>
+            </select></br>
             <input type="submit" value="Submit" />'''
         page_close = '''
         </form>
@@ -42,7 +42,7 @@ class MainHandler(webapp2.RequestHandler):
             <p>Age: ''' + age + '''</p>
             <p>Interest: ''' + interest + '''</p>''' + page_close)
         else:
-            self.response.write(page_head + page_body + page_close)
+            self.response.write(page_head + page_form + page_close)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
