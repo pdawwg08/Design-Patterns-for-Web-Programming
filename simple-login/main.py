751525f36e93b71632cfa63fd9e47d3a3c64e598
pdawwg08 <pdawwg08@yahoo.com>
2015-02-12 00:47:55 -0700

    setting up launcher

---
+++ simple-login/main.py
@@ -0,0 +1,14 @@
+'''
+Preston Cain
+2/12/15
+DPW Launcher
+'''
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
