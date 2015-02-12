49311ef23e78c14c2fd6a04730366dd88dd26904
pdawwg08 <pdawwg08@yahoo.com>
2015-02-12 14:18:23 -0700

    form video follow along

--- simple-login/main.py
+++ simple-login/main.py
@@ -1,13 +1,32 @@
 '''
 Preston Cain
 2/12/15
-DPW Launcher
+Simple Form
 '''
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        page_head = '''<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Simple Form</title>
+    </head>
+    <body>'''
+        page_body = '''<form method="GET">
+            <label>Name: </label><input type="text" name="user"/>
+            <label>Email: </label><input type="text" name="email"/>
+            <input type="submit" value="Submit" />'''
+        page_close = '''
+        </form>
+    </body>
+</html>'''
+        if self.request.GET:
+            user = self.request.GET['user']
+            email = self.request.GET['email']
+            self.response.write(page_head + user + ' ' + email + page_close)
+        else:
+            self.response.write(page_head + page_body + page_close)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
