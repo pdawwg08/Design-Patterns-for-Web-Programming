a78235cd26c04c7120bca80339b1f9c6eb880602
pdawwg08 <pdawwg08@yahoo.com>
2015-02-25 22:47:24 -0700

    add average function

--- reusable_library/library.py
+++ reusable_library/library.py
@@ -2,7 +2,7 @@ class ComicData(object): #data object
     def __init__(self):
         self.series_title =""
         self.month = ""
-        self.issue_title = ""
+        self.__issue_title = ""
         self.issue_number = 0
         self.writer = ""
 class ComicCollection(object):
@@ -16,3 +16,12 @@ class ComicCollection(object):
         for comic in self.__comic_list:
             output += "<p>Issue: " + comic.issue_title + " (" + comic.month +")<br />" + comic.series_title + " " + str(comic.issue_number) + ", " + comic.writer + "</p>"
         return output
+    def average_comic_number(self):
+        average = 0
+        total = 0
+        comics = 0
+        for comic in self.__comic_list:
+            total += comic.issue_number
+            comics += 1
+        average = total / comics
+        return average
