869e7e9583b6ba11e1de02f3fd289c4afc4adffe
pdawwg08 <pdawwg08@yahoo.com>
2015-02-26 00:38:56 -0700

    inheritance code

--- inheritance/main.py
+++ inheritance/main.py
@@ -1,24 +1,49 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        p = FormPage()
+        p.inputs = [["first_name","text","First Name"],["last_name","text","Last Name"],["Submit","submit"]]
+        self.response.write(p.print_out_form())
+        
+class Page(object):
+    def __init__(self):
+        self._head = """
+<!doctype html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>"""
+        
+        self._body = "Filler"
+        self._close = """
+    </body>
+</html>"""
+        
+    def print_out(self):
+        return self._head + self._body + self._close
+class FormPage(Page):
+    def __init__(self):
+        super(FormPage, self).__init__()
+        self._form_open = '<form method="GET">'
+        self._form_close = "</form>"
+        self.__inputs = []
+        self._form_inputs = ""
+    @property
+    def inputs(self):
+        pass
+    @inputs.setter
+    def inputs(self, arr):
+        self.__inputs = arr
+        for item in arr:
+            self._form_inputs += '<input type="' + item[1] +'" name ="' + item[0]
+            try:
+                self._form_inputs += '"placeholder="' + item[2] +'" />'
+            except:
+                self._form_inputs += '" />'
+    def print_out_form(self):
+        return self._head + self._body + self._form_open + self._form_inputs + self._form_close + self._close
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
