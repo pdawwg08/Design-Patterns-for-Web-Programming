dc4cad12ba23310a4bcda6c8ea53106cf949650f
pdawwg08 <pdawwg08@yahoo.com>
2015-02-28 20:32:28 -0700

    remove old code

--- dynamic site/pages.py
+++ dynamic site/pages.py
@@ -21,23 +21,7 @@ class CreatePage(Page):
         super(CreatePage, self).__init__()
         self.css = "css/style.css"
         self.title = "Welcome"
-        self._form_open = '<form method="GET">'
-        self._form_close = "</form>"
-        self.__inputs = []
-        self._form_inputs = ""
-    @property
-    def inputs(self):
-        pass
-    @inputs.setter
-    def inputs(self, arr):
-        self.__inputs = arr
-        for item in arr:
-            self._form_inputs += '<input type="' + item[1] +'" name ="' + item[0]
-            try:
-                self._form_inputs += '"placeholder="' + item[2] +'" />'
-            except:
-                self._form_inputs += '" />'
     def print_out(self):
-        all = self._head + self._body + self._form_open + self._form_inputs + self._form_close + self._close
+        all = self._head + self._body + self._close
         all = all.format(**locals())
         return all
