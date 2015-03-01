2bf9750e1e1b19a1458d34b857e245a487dcf998
pdawwg08 <pdawwg08@yahoo.com>
2015-02-28 20:43:49 -0700

    remove old inputs

--- dynamic site/main.py
+++ dynamic site/main.py
@@ -5,7 +5,6 @@ from pages import Page, CreatePage
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = CreatePage()
-        p.inputs = [["first_name","text","First Name"],["last_name","text","Last Name"],["Submit","submit"]]
         self.response.write(p.print_out())
 
 app = webapp2.WSGIApplication([
